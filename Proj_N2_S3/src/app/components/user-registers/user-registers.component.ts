58a6e5b85d6ab7a7f1f6d07bc17aae915006da0d
Christian <112213507+LuccaChris@users.noreply.github.com>
2024-09-25 20:51:37 -0300

    Initial commit

---
+++ Proj_N2_S3/src/app/components/user-registers/user-registers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registers',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-registers.component.html',
+  styleUrl: './user-registers.component.css'
+})
+export class UserRegistersComponent {
+
+}
