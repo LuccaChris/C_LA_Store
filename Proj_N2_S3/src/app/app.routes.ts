28d89a5f6776ddbc6d1126fed713aaede1a4f3dd
Christian <christian.nar@hotmail.com>
2024-09-28 19:52:03 -0300

    01
    10

--- Proj_N2_S3/src/app/app.routes.ts
+++ Proj_N2_S3/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserRegistersComponent } from './components/user-registers/user-registers.component';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'registrodeusuarios', component:UserRegistersComponent}
+];
