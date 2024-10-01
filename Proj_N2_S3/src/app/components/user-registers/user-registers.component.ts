fb60a099ac567ad97f5d2f8fc6a83059500d1ea8
Christian <christian.nar@hotmail.com>
2024-10-01 15:14:45 -0300

    1/10

--- Proj_N2_S3/src/app/components/user-registers/user-registers.component.ts
+++ Proj_N2_S3/src/app/components/user-registers/user-registers.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input'
+import {MatIconModule}from '@angular/material/icon'
+import {MatDividerModule}from'@angular/material/divider'
+import {MatButtonModule}from '@angular/material/button'
+import{MatCardModule}from '@angular/material/card'
+
 
 @Component({
   selector: 'app-user-registers',
   standalone: true,
-  imports: [MatSidenavModule,FormsModule,MatInputModule],
+  imports: [MatSidenavModule,
+            FormsModule,
+            MatInputModule,
+            MatIconModule,
+            MatButtonModule,
+            MatDividerModule,
+            MatCardModule],
   templateUrl: './user-registers.component.html',
   styleUrl: './user-registers.component.css'
 })
