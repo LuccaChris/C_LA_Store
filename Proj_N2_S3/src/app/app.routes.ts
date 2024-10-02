43597fa2cc9ab77c96642f40995205d1a0fb46dd
Christian <christian.nar@hotmail.com>
2024-10-01 23:18:41 -0300

    form do estoque

--- Proj_N2_S3/src/app/app.routes.ts
+++ Proj_N2_S3/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { UserRegistersComponent } from './components/user-registers/user-registers.component';
 import { LoginComponent } from './components/login/login.component';
+import { EstoqueComponent } from './components/estoque/estoque.component';
 import path from 'path';
 
 export const routes: Routes = [
     {path: 'registrodeusuarios', component:UserRegistersComponent},
-    {path: 'login',component:LoginComponent}
+    {path: 'login',component:LoginComponent},
+    {path: 'estoque',component:EstoqueComponent}
 ];
--- Proj_N2_S3/src/app/components/estoque/estoque.component.ts
+++ Proj_N2_S3/src/app/components/estoque/estoque.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input'
+import {MatIconModule}from '@angular/material/icon'
+import {MatDividerModule}from'@angular/material/divider'
+import {MatButtonModule}from '@angular/material/button'
+import{MatCardModule}from '@angular/material/card'
 
 @Component({
   selector: 'app-estoque',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatCardModule],
   templateUrl: './estoque.component.html',
   styleUrl: './estoque.component.css'
 })
